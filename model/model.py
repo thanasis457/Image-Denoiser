5a09f0bb56d91a151e221fd7e1cb3ab8ee293342
sripushkar <sripushkar@gatech.edu>
2025-11-28 21:31:38 -0500

    add images and stuff

---
+++ model/model.py
@@ -0,0 +1,7 @@
+from __future__ import annotations
+
+
+def corrupt_image(image_bytes: bytes) -> bytes:
+    """Placeholder corrupt function that simply echoes the provided image."""
+    return image_bytes
+
