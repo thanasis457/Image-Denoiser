c126a46244c7c83eb692cfe2d3a858884ca9f3ab
Athanasios Taprantzis <thatapras@hotmail.gr>
2026-01-22 01:34:51 +0200

    Clean Up

--- model/model.py
+++
@@ -1,7 +0,0 @@
-from __future__ import annotations
-
-
-def corrupt_image(image_bytes: bytes) -> bytes:
-    """Placeholder corrupt function that simply echoes the provided image."""
-    return image_bytes
-
